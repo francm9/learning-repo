d1443ddc4ff71623be6ec56d8866fd04459c29a0
francmUMA <francano368@gmail.com>
2023-01-25 16:55:51 +0100

    Minesweeper finished

--- Python/Curso IA CS50/Buscaminas/game.py
+++ Python/Curso IA CS50/Buscaminas/game.py
@@ -4,7 +4,7 @@ from table_generator import generar_tablero
 tablero = generar_tablero()
 
 #Metodo que abre una celda
-def abrir_celda(x, y):
+def abrir_celda(x, y, counter):
     #Si es una bomba pierde la partida
     if tablero[x][y][0] == -1:
         tablero[x][y] = (-1, 'x')
@@ -13,6 +13,7 @@ def abrir_celda(x, y):
     else:
         if tablero[x][y][1] != "ob":
             tablero[x][y] = (tablero[x][y][0], 'x')
+            counter += 1
             if tablero[x][y][0] == 0:
                 for i in range(x-1,x+2):
                     #Comprobamos que sea una fila válida
@@ -22,8 +23,8 @@ def abrir_celda(x, y):
                             if j >= 0 and j <= 9:
                                 #Comprobamos que la celda no se haya abierto y que no sea una bandera
                                 if tablero[i][j][1] == "o":
-                                    abrir_celda(i, j) 
-
+                                    counter = abrir_celda(i, j, counter)  
+    return counter
 #Metodo que muestra el tablero
 def mostrar_tablero():
     #Mostrar el numero de columna
@@ -50,17 +51,36 @@ def mostrar_tablero():
     print("+---------------------+")
 
 def play():
+    celdas_counter = 0
     fin = False
     while not fin:
+
+        #Introduce una accion correcta
         action = int(input("0: Abrir celda\n1: Colocar bandera\nElige la acción a realizar: "))
+        while action > 1 or action < 0:
+            action = int(input("El dato que has introducido en el mundo es incorrecto. Introducelo de nuevo: "))
+
+        #Introduce una fila correcta    
         x = int(input("Introduce la fila: "))
+        while x > 9 or action < 0:
+            x = int(input("El dato que has introducido en el mundo es incorrecto. Introducelo de nuevo: "))
+
+        #Introduce una columna correcta
         y = int(input("Introduce la columna: "))
+        while y > 9 or action < 0:
+            y = int(input("El dato que has introducido en el mundo es incorrecto. Introducelo de nuevo: "))
+        
+        #Se ejecuta la accion introducida    
         if action:
             colocar_bandera(x, y)
         else:
-            abrir_celda(x, y)
+            celdas_counter = abrir_celda(x, y, celdas_counter)
+
+        #Se muestra el tablero    
         mostrar_tablero()
-        fin = check_fin(x, y)
+
+        #Se comprueba si ha finalizado la partida
+        fin = check_fin(x, y, celdas_counter)
 
 
 #Metodo que se utiliza para colocar una bandera
@@ -69,8 +89,10 @@ def colocar_bandera(x, y):
 
 #Comprueba cuando acaba la partida, es decir, que se hayan abierto todas las celdas menos las de las bombas o que 
 #se haya abierto una celda con bomba
-def check_fin(x, y):
-    fin = (tablero[x][y][0] == -1 and tablero[x][y][1] == 'x')
+def check_fin(x, y, counter):
+    fin = (tablero[x][y][0] == -1 and tablero[x][y][1] == 'x') or counter == 90
+    if counter == 90:
+        print("You win")
     return fin
 
 play()

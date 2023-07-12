3889739b692fc59c6799bc8771fed84a4f0a12a1
francm <francano368@uma.es>
2023-07-12 21:13:39 +0200

    traits

---
+++ rust/9.TTL/ttl/src/main.rs
@@ -0,0 +1,11 @@
+fn largest<T, K>(p1: T, p2: K) -> T {
+    p1
+}
+
+fn largest_f32(p1: f32) -> f32{
+    p1
+}
+
+fn main() {
+    
+}
---
+++ rust/9.TTL/ttl/target/debug/deps/ttl-72d5263829ff7538.d
@@ -0,0 +1,5 @@
+/home/devfran/dev/rust/9.TTL/ttl/target/debug/deps/ttl-72d5263829ff7538.rmeta: src/main.rs
+
+/home/devfran/dev/rust/9.TTL/ttl/target/debug/deps/ttl-72d5263829ff7538.d: src/main.rs
+
+src/main.rs:

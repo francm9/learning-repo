87dc7c7cfb2eb93591d8d53a7d9c29c7db53d33e
francm9 <francano368@uma.es>
2024-09-14 20:51:35 +0200

    init branch with main

---
+++ Python/bank-scrapper/main.py
@@ -0,0 +1,10 @@
+"""
+Este proyecto pretende scrappear la cuenta del banco para obtener los movimientos de la cuenta y poder tener
+un control automatico de tus finanzas.
+
+# DOCUMENTACION
+- API3: https://api3-explorer.openbankproject.com/?tags=&locale=en_GB
+
+# TAREAS
+
+"""

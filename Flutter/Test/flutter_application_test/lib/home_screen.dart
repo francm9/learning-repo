c1fcaf6e371005d0cb32b11801142cf89c46f7a6
francm9 <francano368@uma.es>
2024-10-06 21:03:58 +0200

    maquetacion de app

--- Flutter/Test/flutter_application_test/lib/home_screen.dart
+++ Flutter/Test/flutter_application_test/lib/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'footer.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -11,49 +10,55 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   @override
-  Widget build(BuildContext context) 
-  {
-    return Scaffold
-    (
-      appBar: AppBar(),
-      body: const Column
-      (
-        children: 
-        [
-          Expanded
-          (
-            child: Column
+ Widget build(BuildContext context) {
+    return Scaffold(
+      body: Expanded(
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+          
+            Container(
+              height: 250,
+              decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                  colors: [
+                    Color(0xFF9D50BB), 
+                    Color(0xFF6E48AA), 
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+      
+            Positioned
             (
-              children: 
-              [
-                Row
-                (
-                  children: 
+              top: 210,
+              left: 80,
+              child: 
+              
+              Container
+              (
+                width: 250, 
+                height: 70, 
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10), 
+                  boxShadow: 
                   [
-                    Padding
-                    (
-                      padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
-                      child: Text
-                      (
-                        'Bienvenida de nuevo piti', 
-                        style: TextStyle
-                        (
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.2),
+                      spreadRadius: 5,
+                      blurRadius: 5,
                     ),
                   ],
                 ),
-                Divider()
-              ],
+              ),
             ),
-          ),
-          Footer
-          (
-            focusIcon: 0
-          ),
-        ],
+            
+          ],
+        ),
       ),
     );
   }

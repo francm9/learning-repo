fe6819ceac4eb8a8d66d3e0a09405e4a383872fa
francm9 <francano368@uma.es>
2024-10-12 14:03:45 +0200

    Maqueta hecha

--- Flutter/Test/flutter_application_test/lib/home_screen.dart
+++ Flutter/Test/flutter_application_test/lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_test/utils/appcolors.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -10,18 +11,24 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   @override
- Widget build(BuildContext context) {
+ Widget build(BuildContext context) 
+ {
     return Scaffold(
-      body: Expanded(
-        child: Stack(
+      body: Expanded
+      (
+        child: Stack
+        (
           clipBehavior: Clip.none,
-          children: [
-          
+          children: 
+          [
             Container(
-              height: 250,
-              decoration: BoxDecoration(
-                gradient: const LinearGradient(
-                  colors: [
+              height: 315,
+              decoration: BoxDecoration
+              (
+                gradient: const LinearGradient
+                (
+                  colors: 
+                  [
                     Color(0xFF9D50BB), 
                     Color(0xFF6E48AA), 
                   ],
@@ -34,16 +41,86 @@ class _MyHomePageState extends State<MyHomePage> {
       
             Positioned
             (
-              top: 210,
-              left: 80,
-              child: 
-              
-              Container
+              top: 284,
+              left: 72,
+              child: Container
               (
-                width: 250, 
-                height: 70, 
+                width: 249, 
+                height: 63, 
                 decoration: BoxDecoration(
-                  color: Colors.white,
+                  color: AppColors.pink50,
+                  borderRadius: BorderRadius.circular(10), 
+                  boxShadow: 
+                  [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.2),
+                      spreadRadius: 5,
+                      blurRadius: 5,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            Positioned
+            (
+              top: 390,
+              left: 15,
+              child: Container
+              (
+                width: 363, 
+                height: 169, 
+                decoration: BoxDecoration
+                (
+                  color: AppColors.pink50,
+                  borderRadius: BorderRadius.circular(10), 
+                  boxShadow: 
+                  [
+                    BoxShadow
+                    (
+                      color: Colors.grey.withOpacity(0.2),
+                      spreadRadius: 5,
+                      blurRadius: 5,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            Positioned
+            (
+              top: 593,
+              left: 15,
+              child: Container
+              (
+                width: 151, 
+                height: 231, 
+                decoration: BoxDecoration(
+                  color: AppColors.pink50,
+                  borderRadius: BorderRadius.circular(10), 
+                  boxShadow: 
+                  [
+                    BoxShadow
+                    (
+                      color: Colors.grey.withOpacity(0.2),
+                      spreadRadius: 5,
+                      blurRadius: 5,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            Positioned
+            (
+              top: 593,
+              left: 226,
+              child: Container
+              (
+                width: 152, 
+                height: 231, 
+                decoration: BoxDecoration(
+                  color: AppColors.pink50,
                   borderRadius: BorderRadius.circular(10), 
                   boxShadow: 
                   [
